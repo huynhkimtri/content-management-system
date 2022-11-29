2368df056b3a4e280eee0000bc269efce78fa8a1
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2022-11-30 01:43:14 +0700

    Add sample database migration file

---
+++ database/migrations/2022.11.30T00.00.00.sample-migration.ts
@@ -0,0 +1,23 @@
+'use strict'
+/**
+ * Strapi automatically detects migration files and run them once at the next startup in alphabetical order. 
+ * Every new file is executed once. 
+ * Migrations are run before the database tables are synced with the content-types schemas.
+ */
+export default {
+  async up(knex) {
+    // You have full access to the Knex.js API with an already initialized connection to the database
+    // https://knexjs.org/guide/
+
+    // EXAMPLE: renaming a table
+    await knex.schema.renameTable('oldName', 'newName');
+
+    // EXAMPLE: renaming a column
+    await knex.schema.table('someTable', table => {
+      table.renameColumn('oldName', 'newName');
+    });
+
+    // EXAMPLE: updating data
+    await knex.from('someTable').update({ columnName: 'newValue' }).where({ columnName: 'oldValue' });
+  },
+};
